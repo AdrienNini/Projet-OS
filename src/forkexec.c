ef52e3faa4d205e5905b3ed4c542cc5e27a36bd1
Adrien Nini Pereira <Adrien@mbpdeadriennini.home>
2017-05-04 21:11:46 +0200

    Correction de bug write/read avec strlen

--- src/forkexec.c
+++ src/forkexec.c
@@ -1,25 +1,51 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 
 int main(int argc, char **argv){
 
 	int pf[2];
 	int fp[2];
+	int vr;
 
 	if (pipe(pf) == -1){
-		printf("An error occured during the creation of the pipe\n");
+		char error[] = "An error occured during the creation of the pipe\n";
+		write(2, error, strlen(error));
 	}
 
 	if (pipe(fp) == -1){
-		printf("An error occured during the creation of the pipe\n");
+		char error[] = "An error occured during the creation of the pipe\n";	
+		write(2, error, strlen(error));
 	}
 
-	if (fork() == 0) {
-		char buffer[50];
-		read(pf[0], buffer, sizeof(argv[1]));
-		printf("%s\n", buffer);
+	if ((vr = fork()) == -1){
+		char error[] = "An error occured during the creation of the pipe\n";
+		write(2, error, strlen(error));
+	}
+
+	 if (vr == 0) {
+		char buffer[255];
+		int nbRead;
+		if ((nbRead = read(pf[0], buffer, strlen(argv[1]))) == strlen(argv[1])){
+			printf("Child : %s\n", buffer);
+			write(fp[1], buffer, nbRead);
+		} else {
+			char error[] = "Error";
+			write(2, error, strlen(error));
+		}
+
+		exit(0);
 	} else {
-		write(pf[1], argv[1], sizeof(argv[1]));
+		char buffer[255];
+		int nbRead, nbWrite;
+
+		nbWrite = write(pf[1], argv[1], strlen(argv[1]));
+		printf("Parent : %s\n", argv[1]);
+		nbRead = read(fp[0], buffer, nbWrite);
+		write(1, buffer, nbRead);
+
+		exit(0); 
 	}
 
 	return 0;
