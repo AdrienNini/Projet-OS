c4eb7a40255ee63dcb51fd0e3854828aa31bbb69
Adrien Nini Pereira <adrien97nini@gmail.com>
2017-05-04 12:52:39 +0200

    Envoie une chaine de caractère via le pipe et child l'affiche

--- src/forkexec.c
+++ src/forkexec.c
@@ -1,15 +1,25 @@
 #include <stdio.h>
 #include <unistd.h>
 
-int main(){
+int main(int argc, char **argv){
 
 	int pf[2];
-	int fp;
+	int fp[2];
 
 	if (pipe(pf) == -1){
-		printf("error\n");
+		printf("An error occured during the creation of the pipe\n");
+	}
+
+	if (pipe(fp) == -1){
+		printf("An error occured during the creation of the pipe\n");
+	}
+
+	if (fork() == 0) {
+		char buffer[50];
+		read(pf[0], buffer, sizeof(argv[1]));
+		printf("%s\n", buffer);
 	} else {
-		printf("success !\n");
+		write(pf[1], argv[1], sizeof(argv[1]));
 	}
 
 	return 0;
