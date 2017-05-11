5990b467eab71c6d6d3e00742bfd3477cd74c605
Adrien Nini Pereira <adrien97nini@gmail.com>
2017-05-11 12:51:20 +0200

    Suppression des pipes non necéssaires

--- src/forkexec.c
+++ src/forkexec.c
@@ -7,18 +7,10 @@ int main(int argc, char **argv){
 
 	// VARIABLES	
 
-	int pf[2]; // Pipe Père -> Fils
 	int fp[2]; // Pipe Fils -> Père
 	int vr; // Résultat du fork()
 
 	// PROGRAM
-
-	if (pipe(pf) == -1){ // Crée et vérifie que la création du pipe n'a pas créé d'érreur
-
-		char error[] = "An error occured during the creation of the pipe\n"; 
-		write(2, error, strlen(error)); // Affiche l'érreur sur STDERR
-
-	}
 
 	if (pipe(fp) == -1){
 		
@@ -35,19 +27,13 @@ int main(int argc, char **argv){
 
 		char buffer[255];
 		int nbWrite; // Taille de la chaine de caractère envoyée dans le pipe
-		int nbRead; // Taille de la chaine de caractère passée dans le pipe
 
-		if ((nbRead = read(pf[0], buffer, strlen(argv[1]))) == strlen(argv[1])){ // SI la chaine récupérée dans le pipe à la même taille que la chaine passée en argument -> OK
 			if ((nbWrite = write(fp[1], buffer, nbRead)) == nbRead) { // Envoie la chaine du Buffer dans le Pipe FP
 				
 			} else {
 				char error[] = "String length failure\n";
 				write(2, error, strlen(error)); // Affiche une érreur sur SDTERR
 			}	
-		} else {
-			char error[] = "String length failure\n";
-			write(2, error, strlen(error)); // Affiche une érreur sur SDTERR
-		}
 
 		exit(0);
 
@@ -55,16 +41,13 @@ int main(int argc, char **argv){
 
 		char buffer[255];
 		int nbRead; //Taille de la chaine de caractère passée dans le pipe
-		int nbWrite; // Taille de la chaine de caractère envoyé dans le pipe
 
-		if ((nbWrite = write(pf[1], argv[1], strlen(argv[1]))) == strlen(argv[1])) { // Envoie la chaine passée en argument dans le Pipe PF
 			if ((nbRead = read(fp[0], buffer, nbWrite)) == nbWrite) {	
 				write(1, buffer, nbRead); // Ecrit le buffer sur STDOUT
 			} else {
 				char error[] = "String length failure\n";
 				write(2, error, strlen(error));
 			}
-		}
 
 		exit(0); 
 	}
