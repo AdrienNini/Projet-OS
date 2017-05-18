e302d3143275907606204cba802c3c7f08e86932
Adrien Nini Pereira <adrien97nini@gmail.com>
2017-05-18 12:48:13 +0200

    enfant exécute une commande

--- src/forkexec.c
+++ src/forkexec.c
@@ -7,7 +7,7 @@ int main(int argc, char **argv){
 
 	// VARIABLES	
 
-	int pf[2]; // Pipe Père -> Fils
+	int fp[2]; // Pipe Père -> Fils
 	int vr; // Résultat du fork()
 
 	// PROGRAM
@@ -19,7 +19,7 @@ int main(int argc, char **argv){
 
 	}
 
-	if (pipe(pf) == -1){ // Crée et vérifie que la création du pipe n'a pas créé d'érreur
+	if (pipe(fp) == -1){ // Crée et vérifie que la création du pipe n'a pas créé d'érreur
 
 		char error[] = "An error occured during the creation of the pipe\n"; 
 		write(2, error, strlen(error)); // Affiche l'érreur sur STDERR
@@ -32,22 +32,13 @@ int main(int argc, char **argv){
 		write(2, error, strlen(error)); // Affiche l'érreur sur STDERR
 
 	} else if (vr == 0) { // Process Fils
-		char buffer [255]; 
-		int nbWrite; // taille de la cahine de caractère envoyé dans le pipe
-		nbWrite = write(pf[1], argv[1], strlen(argv[1])); // Envoie la cahine passée en argument dans le pipe PF 
+		char *args[] = { argv[1], argv[2], argv[3], NULL};
+		execvp(argv[1], args);
 	}
 	else { // Process Père 
 
 		char buffer[255];
 		int nbRead; // Taille de la chaine de caractère passée dans le pipe
-
-		if ((nbRead = read(pf[0], buffer, strlen(argv[1])))){ // Lecture du Pipe 
-			write(1, buffer, nbRead); // Affiche la chaine du Buffer sur STDOUT
-			
-		} else {
-			char error[] = "String length failure\n";
-			write(2, error, strlen(error)); // Affiche une érreur sur SDTERR
-		}
 
 		exit(0);
 
